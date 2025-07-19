6ebecb38263007e0087fc584cb36d996eabcb4e3
Firas Dhaouadi <firasdhaouadi@edtrust.tn>
2025-07-19 16:27:46 +0100

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// Replace this with your repo name
-const repoName = "Betawaves_cms";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Betawaves_cms/' : '/',
+  base: '/',
   server: {
     host: "::",
     port: 2100,
